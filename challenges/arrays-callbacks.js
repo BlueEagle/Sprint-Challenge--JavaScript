3b41f8f6fb335f039dfe5b41518b0db51debe6f2
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-05-22 09:43:53 -0600

    task_1 step_2

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -69,7 +69,9 @@ function consume(a, b, cb) {
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
+const add = (num1, num2) => num1 + num2;
+const multiply = (num1, num2) => num1 * num2;
+const greeting = (firstName, lastName) => `Hello ${firstName} ${lastName}, nice to meet you!`;
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // console.log(consume(2, 2, add)); // 4
